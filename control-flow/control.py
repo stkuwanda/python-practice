94564ddd196c4682182c0cdf3e0017a07a143606
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-07-21 19:53:25 +0200

    control file and if statement

---
+++ control-flow/control.py
@@ -0,0 +1,13 @@
+# control flow
+a = 20
+b = 30
+# if condition:
+#     statement_1
+#     statement_2
+
+# if statement
+# all indented statements form part of the if statement as a block
+if a > b:
+    print("a is greater than b")
+    print('Go ahead')
+print('will always run...') # not part of if statement block
