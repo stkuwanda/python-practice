dc036efed4318999a889e98b348c4a6046bb7cf3
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-07-22 03:49:59 +0200

    updated directory structure

--- 3-operators/operators.py
+++ 3-operators/operators.py
@@ -3,7 +3,7 @@ b = 3
 c = 4
 d = 5
 
-# arithmetic operators
+# arithmetic 3-operators
 print('Addition: ', a + b)
 print('Subtraction: ', a - b)
 print('Multiplication: ', a * b)
@@ -12,7 +12,7 @@ print('Floor division: ', a // b)
 print('Exponentiation: ', a ** b)
 print('Modulo: ', a % b)
 
-# assignment operators
+# assignment 3-operators
 c += b
 print(c)
 
@@ -31,7 +31,7 @@ print(c)
 c /= 2
 print(c)
 
-# comparison operators
+# comparison 3-operators
 print('equivalence: a == b =', a == b)
 print('non-equivalence: a != b =', a != b)
 print('greater: a > b =', a > b)
@@ -39,19 +39,19 @@ print('less: a < b =', a < b)
 print('less_equal: a <= b =', a <= b)
 print('greater_equal: a >= b =', a >= b)
 
-# logical operators
+# logical 3-operators
 print('and operator: ', a < b and a < 10)
 print('or operator: ', a <= b or a <= 10)
 print('not operator: ', not a != b)
 
-# membership operators
+# membership 3-operators
 string1 = 'Hello'
 items = [1, 2, 3]
 print("'H' in string1: ", 'H' in string1)
 print('3 in items: ', 3 in items)
 print('10 not in items: ', 10 not in items)
 
-# identity operators
+# identity 3-operators
 z = 10
 print('a is b: ', a is b)
 print('a is not b: ', a is not b)
