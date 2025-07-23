2a90d610161aad240a834405106a846b9a6d05ef
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-07-23 09:54:51 +0200

    string manipulation - indexing and slicing

---
+++ 8-string-manipulation/string-manipulation.py
@@ -0,0 +1,17 @@
+# string indexing and slicing
+# positive indices start from 0 and increment by 1
+# negative indices start from -1 and decrement by 1
+str1 = 'Hello World'
+print('should print o:', str1[4])
+print('should print o:', str1[-7])
+
+# slicing
+print('substring from index 2 to index (5-1):', str1[2:5])
+print('origin string variable str1:', str1)
+
+# enhance slicing by introducing a custom increment value
+# this will output values within the range and selected
+# by the increment or skip magnitude
+print('a strange substring:', str1[2:10:2])
+print('another strange substring:', str1[-1:-12:-2])
+print('another strange substring:', str1[0::2]) # the missing value implies the whole range is included to the last index
