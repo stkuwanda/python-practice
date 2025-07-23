579c8cbd79eee25456f6da66817420d47fd2b1f0
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-07-23 12:20:24 +0200

    iterating through a string

--- 8-string-manipulation/string-manipulation.py
+++ 8-string-manipulation/string-manipulation.py
@@ -15,3 +15,25 @@ print('origin string variable str1:', str1)
 print('a strange substring:', str1[2:10:2])
 print('another strange substring:', str1[-1:-12:-2])
 print('another strange substring:', str1[0::2]) # the missing value implies the whole range is included to the last index
+
+# string iteration
+str1 = 'Welcome to python programming language'
+
+# get string length
+l = len(str1)
+print('String length:', l)
+
+# iterate through a string
+for n in range(l):
+    print(str1[n])
+
+# reverse a string
+str2 = str1[-1::-1]
+print('reversed string:')
+for n in range(len(str2)):
+    print(str2[n])
+
+# the string is an iterable sequence type in itself
+print('iterate through a string')
+for n in str1:
+    print(n)
