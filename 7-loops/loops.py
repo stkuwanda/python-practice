0bbefa44835663ebafb14e07114ddc40e859437e
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-08-11 08:22:48 +0200

    added comments on usage of the range func

--- 7-loops/loops.py
+++ 7-loops/loops.py
@@ -40,3 +40,14 @@ for i in range(100, 0, -1):
 for i in range(1, 101, 2):
     print(i)
 
+# range() func generates a sequence of numbers. It’s commonly used with for
+# loops to repeat actions a specific number of times.
+# range() returns a range object, which is lazy — it doesn’t generate all numbers at once.
+print('range func returns a range obj:', range(5))
+# a range object can be converted to a list using a list func
+print('convert a range obj to a list:', list(range(5)))
+
+# use range func in the following ways
+# range(stop)               # starts at 0, ends before stop
+# range(start, stop)        # starts at start, ends before stop
+# range(start, stop, step)  # starts at start, ends before stop, increments by step
